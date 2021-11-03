159acbb452563b9bd40a6eedd33e463fc71c81b2
akpojotor-shemi <akpojotor.shemi@ibm.com>
2021-11-02 21:06:33 -0400

    update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return render_template("index.html")
-    return "This is an BSOK python app."
+    return "This is an ajs webhook test of  python app."
  
 
 @app.route("/healthz")
